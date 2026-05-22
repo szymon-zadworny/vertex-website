060d91899fb4ff9dcb67a627480c7468051c49b1
Natalia <nata18629@gmail.com>
2026-05-22 23:01:17 +0200

    Article content md to html conversion

--- src/converter.py
+++ src/converter.py
@@ -1,8 +1,9 @@
 import os
 import datetime
+import mistune
 
 
-PATH = "src/shit"
+PATH = "shit"
 
 
 class Article:
@@ -14,25 +15,26 @@ class Article:
         self.date = date
         self.title = title
         self.content_path = content_path
+    
 
+    def convert_to_html(self):
+        file = open(self.content_path)
+        html = mistune.html(file.read())
+        return html
 
 
 def get_articles():
     articles = []
-
     dirs = [os.path.join(PATH, f) for f in os.listdir(PATH)]
-
 
     for dir in dirs:
         if not os.path.isdir(dir):
             continue
-
         posts = [os.path.join(dir, f) for f in os.listdir(dir)]
 
         for post in posts:
             if not os.path.isdir(post):
                 continue
-
             date = os.path.basename(dir)
             date = date.split('-')
             date = datetime.datetime(int(date[0]), int(date[1]), int(date[2]))
@@ -41,7 +43,6 @@ def get_articles():
             title = " ".join(title)
             content_path = os.path.join(post, os.path.basename(post))
             content_path = "".join([content_path, ".md"])
-
             article = Article(date, title, content_path)
             articles.append(article)
     
