76cbe801bafadbf47d60828105abeb70c48fdd6e
sajmon <szymon17099@gmail.com>
2026-05-26 18:15:14 +0200

    Read blog articles

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 from asgiref.wsgi import WsgiToAsgi
 from flask import Flask, render_template, url_for, send_from_directory
+from converter import get_articles
 
 app = Flask(__name__)
 asgi_app = WsgiToAsgi(app)
+
+blog = get_articles("content/blog")
 
 @app.route("/")
 def index():
